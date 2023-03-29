Model Averagers added Bug in SnapshotWeightedModel solved

src/experiments/ComparingModelsOnRealData.py
@@ -1,6 +1,7 @@
 import logging
 import os.path
 import time
+from functools import partial
 
 import joblib
 import numpy as np
@@ -18,12 +19,13 @@ from src.lib.DataProcessing.PollutionPreprocess import get_pollution, get_statio
 from src.lib.DataProcessing.Prepare4Experiments import get_traffic_pollution_data_per_hour
 from src.lib.DataProcessing.TrafficProcessing import save_load_traffic_by_pixel_data, get_traffic_pixel_coords, \
     load_background
-from src.lib.Models.BaseModel import BaseModel, split_by_station, Bounds, mse, UNIFORM, ModelsSequenciator
+from src.lib.Models.BaseModel import BaseModel, split_by_station, Bounds, mse, UNIFORM, ModelsSequenciator, \
+    ModelsAverager
 from src.lib.Models.TrueStateEstimationModels.AverageModels import SnapshotMeanModel, GlobalMeanModel, \
     SnapshotWeightedModel
 from src.lib.Models.TrueStateEstimationModels.TrafficConvolution import TrafficMeanModel, TrafficConvolutionModel, \
     gaussker
-from src.performance_utils import timeit
+from src.performance_utils import timeit, NamedPartial
 from src.viz_utils import save_fig, generic_plot
 
 
@@ -173,7 +175,7 @@ if __name__ == "__main__":
             return {
                 "model_params": model.params,
                 "estimation": estimation,
-                # "error": ((estimation - data_unknown.values.ravel()) ** 2).ravel(),
+                "error": ((estimation - data_unknown.values.ravel()) ** 2).ravel(),
                 "time_to_fit": t_to_fit,
                 "time_to_estimate": t_to_estimate,
             }
@@ -182,74 +184,120 @@ if __name__ == "__main__":
         return decorated_func
 
 
-    models = [
-        SnapshotWeightedModel(summary_statistic="mean"),
+    models_first = [
+        SnapshotWeightedModel(positive=True, fit_intercept=True),
+        SnapshotWeightedModel(positive=False, fit_intercept=True),
+        ModelsSequenciator(models=[GlobalMeanModel(),
+                                   SnapshotWeightedModel(positive=True, fit_intercept=False)],
+                           name="Global_SnapW"),
+        # ModelsAverager(models=[GlobalMeanModel(),
+        #                        TrafficMeanModel(summary_statistic="mean"),
+        #                        TrafficMeanModel(summary_statistic="median"),
+        #                        SnapshotMeanModel(summary_statistic="mean"),
+        #                        SnapshotMeanModel(summary_statistic="median"),
+        #                        SnapshotWeightedModel(positive=True, fit_intercept=True)],
+        #                positive=False, fit_intercept=True),
+        # ModelsAverager(models=[GlobalMeanModel(),
+        #                        TrafficMeanModel(summary_statistic="mean"),
+        #                        TrafficMeanModel(summary_statistic="median"),
+        #                        SnapshotMeanModel(summary_statistic="mean"),
+        #                        SnapshotMeanModel(summary_statistic="median"),
+        #                        SnapshotWeightedModel(positive=True, fit_intercept=True),
+        #                        ],
+        #                positive=True, fit_intercept=True),
+        ModelsAverager(models=[GlobalMeanModel(),
+                               TrafficMeanModel(summary_statistic="mean"),
+                               TrafficMeanModel(summary_statistic="median"),
+                               SnapshotMeanModel(summary_statistic="mean"),
+                               SnapshotMeanModel(summary_statistic="median"),
+                               SnapshotWeightedModel(positive=True, fit_intercept=True),
+                               ],
+                       positive=True, fit_intercept=False),
+        # ModelsAverager(models=[GlobalMeanModel(),
+        #                        TrafficMeanModel(summary_statistic="mean"),
+        #                        TrafficMeanModel(summary_statistic="median"),
+        #                        SnapshotMeanModel(summary_statistic="mean"),
+        #                        SnapshotMeanModel(summary_statistic="median"),
+        #                        SnapshotWeightedModel(positive=True, fit_intercept=True)],
+        #                positive=False, fit_intercept=True, n_alphas=100),
+        # ModelsAverager(models=[GlobalMeanModel(),
+        #                        TrafficMeanModel(summary_statistic="mean"),
+        #                        TrafficMeanModel(summary_statistic="median"),
+        #                        SnapshotMeanModel(summary_statistic="mean"),
+        #                        SnapshotMeanModel(summary_statistic="median"),
+        #                        SnapshotWeightedModel(positive=True, fit_intercept=True),
+        #                        ],
+        #                positive=True, fit_intercept=True, n_alphas=100),
         SnapshotMeanModel(summary_statistic="mean"),
-        # SnapshotMeanModel(summary_statistic="median"),
+        # # SnapshotMeanModel(summary_statistic="median"),
         GlobalMeanModel(),
         TrafficMeanModel(summary_statistic="mean"),
-        # TrafficMeanModel(summary_statistic="median"),
-        ModelsSequenciator(models=[SnapshotMeanModel(summary_statistic="mean"),
-                                   TrafficMeanModel(summary_statistic="mean")],
-                           name="SnapshotPollutionTraffic")
+        # # TrafficMeanModel(summary_statistic="median"),
+        # ModelsSequenciator(models=[SnapshotMeanModel(summary_statistic="mean"),
+        #                            TrafficMeanModel(summary_statistic="mean")],
+        #                    name="SnapshotPollutionTraffic")
     ]
 
-    lab = LabPipeline()
-    lab.define_new_block_of_functions("true_values", llo4test)
-    lab.define_new_block_of_functions("model", *list(map(train_test_model, models)))
-    lab.execute(
-        data_manager,
-        num_cores=num_cores,
-        forget=False,
-        recalculate=False,
-        save_on_iteration=None,
-        station=station_coordinates.columns.to_list()
-    )
+    models_second = [
+        ModelsSequenciator(models=[
+            SnapshotMeanModel(summary_statistic="mean"),
+            TrafficConvolutionModel(conv_kernel=gaussker, normalize=False,
+                                    sigma=Bounds(0, 2 * longer_distance),
+                                    loss=mse, optim_method=UNIFORM, niter=10, verbose=True)],
+            name="SnapTrafficConv"),
+        ModelsAverager(models=[
+            GlobalMeanModel(),
+            TrafficConvolutionModel(conv_kernel=gaussker, normalize=False,
+                                    sigma=Bounds(0, 2 * longer_distance),
+                                    loss=mse, optim_method=UNIFORM, niter=10, verbose=True),
+            TrafficMeanModel(summary_statistic="mean"),
+            TrafficMeanModel(summary_statistic="median"),
+            SnapshotMeanModel(summary_statistic="mean"),
+            SnapshotMeanModel(summary_statistic="median"),
+            SnapshotWeightedModel(positive=True, fit_intercept=True),
+        ],
+            positive=True, fit_intercept=False),
+    ]
 
-    # ----- Plotting results ----- #
+    for models in [models_first]:  # , models_second
+        lab = LabPipeline()
+        lab.define_new_block_of_functions("true_values", llo4test)
+        lab.define_new_block_of_functions("model", *list(map(train_test_model, models)))
+        lab.execute(
+            data_manager,
+            num_cores=num_cores,
+            forget=True,
+            recalculate=False,
+            save_on_iteration=None,
+            station=station_coordinates.columns.to_list()
+        )
 
-    generic_plot(data_manager, x="station", y="mse", label="model", plot_func=spiderplot,
-                 mse=lambda estimation, future_pollution:
-                 np.sqrt(((estimation - future_pollution.values.ravel()).ravel() ** 2).mean()))
+        # ----- Plotting results ----- #
 
-    generic_plot(data_manager, x="station", y="error", label="model", plot_func=sns.boxenplot,
-                 error=lambda estimation, future_pollution:
-                 np.abs((estimation - future_pollution.values.ravel()).ravel()))
+        generic_plot(data_manager, x="station", y="mse", label="model", plot_func=spiderplot,
+                     mse=lambda error: np.sqrt(error.mean()))
 
-    generic_plot(data_manager, x="model", y="time_to_fit", plot_func=sns.boxenplot)
-    generic_plot(data_manager, x="model", y="time_to_estimate", plot_func=sns.boxenplot)
+        generic_plot(data_manager, x="station", y="l1_error", label="model", plot_func=sns.boxenplot,
+                     sort_by=["mse"],
+                     l1_error=lambda error: np.abs(error).ravel(),
+                     mse=lambda error: np.sqrt(error.mean()))
 
-    dfvbyhe
-    # Conv
-    print(longer_distance)
-    models = [TrafficConvolutionModel(conv_kernel=gaussker, normalize=False, sigma=Bounds(0, 2 * longer_distance),
-                                      loss=mse, optim_method=UNIFORM, niter=10, verbose=True)]
+        generic_plot(data_manager, x="l1_error", y="model", plot_func=NamedPartial(sns.boxenplot, orient="horizontal"),
+                     sort_by=["mse"],
+                     l1_error=lambda error: np.abs(error).ravel(),
+                     mse=lambda error: np.sqrt(error.mean()))
 
-    lab = LabPipeline()
-    lab.define_new_block_of_functions("model", *list(map(train_test_model, models)))
-    lab.execute(
-        data_manager,
-        num_cores=num_cores,
-        forget=False,
-        recalculate=False,
-        save_on_iteration=None,
-        station=station_coordinates.columns.to_list()
-    )
+        generic_plot(data_manager, x="mse", y="model", plot_func=NamedPartial(sns.boxenplot, orient="horizontal"),
+                     sort_by=["mse"],
+                     mse=lambda error: np.sqrt(np.nanmean(error)))
 
-    # ----- Plotting results ----- #
-    generic_plot(data_manager, x="station", y="mse", label="model", plot_func=spiderplot,
-                 mse=lambda estimation, station:
-                 np.sqrt(((estimation - split_by_station(
-                     unknown_station=station, observed_stations=station_coordinates,
-                     observed_pollution=pollution_future, traffic=traffic_future)[1].values[:,
-                                        np.newaxis]).ravel() ** 2).mean()))
+        # generic_plot(data_manager, x="station", y="mse", label="model", plot_func=spiderplot,
+        #              mse=lambda estimation, future_pollution:
+        #              np.sqrt(((estimation - future_pollution.values.ravel()).ravel() ** 2).mean()))
+        #
+        # generic_plot(data_manager, x="station", y="error", label="model", plot_func=sns.boxenplot,
+        #              error=lambda estimation, future_pollution:
+        #              np.abs((estimation - future_pollution.values.ravel()).ravel()))
 
-    generic_plot(data_manager, x="station", y="error", label="model", plot_func=sns.boxenplot,
-                 error=lambda estimation, station:
-                 np.abs((estimation - split_by_station(
-                     unknown_station=station, observed_stations=station_coordinates,
-                     observed_pollution=pollution_future, traffic=traffic_future)[1].values[:,
-                                      np.newaxis]).ravel()))
-
-    generic_plot(data_manager, x="model", y="time_to_fit", plot_func=sns.boxenplot)
-    generic_plot(data_manager, x="model", y="time_to_estimate", plot_func=sns.boxenplot)
+        generic_plot(data_manager, x="model", y="time_to_fit", plot_func=sns.boxenplot)
+        generic_plot(data_manager, x="model", y="time_to_estimate", plot_func=sns.boxenplot)

src/lib/Models/BaseModel.py
@@ -1,13 +1,16 @@
 import logging
 from collections import namedtuple
-from typing import List
+from typing import List, Union, Dict
 
 import cma
 import numpy as np
 import pandas as pd
 from scipy.optimize import minimize
+from sklearn.linear_model import LinearRegression, LassoCV
 from sklearn.model_selection import LeaveOneOut
 from tqdm import tqdm
+
+from src.performance_utils import if_true_str
 
 CMA_OPTIM_METHOD = "cma"
 BFGS_OPTIM_METHOD = "bfgs"
@@ -203,3 +206,78 @@ class ModelsSequenciator:
                                                    for known_data, target_pollution in
                                                    loo(observed_stations, observed_pollution_i, traffic)], axis=1)
         return self
+
+
+class ModelsAverager(BaseModel):
+    def __init__(self, models: List[BaseModel], positive=False, fit_intercept=False,
+                 weights: Union[Dict, List, np.ndarray] = None, name=None, n_alphas=None):
+        super().__init__()
+        self.name = name
+        self.TRUE_MODEL = np.all([model.TRUE_MODEL for model in models])
+
+        self.models = models
+        self.n_alphas = n_alphas
+        if n_alphas is None:
+            self.lr = LinearRegression(positive=positive, fit_intercept=fit_intercept)
+        else:
+            self.lr = LassoCV(positive=positive, fit_intercept=fit_intercept, n_alphas=n_alphas)
+        if weights is not None:
+            if isinstance(weights, Dict):
+                self.lr.coef_ = np.array([weights[str(model)] for model in self.models])
+                self.lr.intercept_ = weights["intercept"]
+            elif isinstance(weights, (List, np.ndarray)):
+                self.lr.coef_ = np.array(weights[:-1])
+                self.lr.intercept_ = weights[-1]
+
+    @property
+    def weights(self):
+        return np.ravel(np.append(self.lr.coef_, self.lr.intercept_))
+
+    @property
+    def model_importance(self):
+        return {model_name: weight for model_name, weight in
+                zip(list(map(str, self.models)) + ["intercept"], np.abs(self.weights) / np.abs(self.weights).sum())}
+
+    @property
+    def params(self):
+        return {**{str(model): model.params for model in self.models}, **{"weights": self.weights.tolist()}}
+
+    def __str__(self):
+        models_names = ','.join([''.join(filter(lambda c: c.isupper(), str(model))) for model in
+                                 self.models]) if self.name is None else self.name
+        return f"{if_true_str(self.lr.fit_intercept, 'c', '', '+')}{'LASSO' if self.n_alphas else 'LR'}{if_true_str(self.lr.positive, '+')}({models_names})"
+
+    def state_estimation_concatenated(self, observed_stations, observed_pollution, traffic,
+                                      target_positions: pd.DataFrame,
+                                      **kwargs) -> np.ndarray:
+        return np.concatenate([
+            model.state_estimation(observed_stations, observed_pollution, traffic, target_positions, **kwargs).reshape(
+                (-1, 1))
+            for model in self.models], axis=1)
+
+    def state_estimation(self, observed_stations, observed_pollution, traffic, target_positions: pd.DataFrame,
+                         **kwargs) -> np.ndarray:
+        individual_predictions = self.state_estimation_concatenated(observed_stations, observed_pollution, traffic,
+                                                                    target_positions, **kwargs)
+        return (individual_predictions @ self.lr.coef_ + self.lr.intercept_).reshape((len(observed_pollution), -1))
+
+    def state_estimation_for_optim(self, observed_stations, observed_pollution, traffic, **kwargs) -> [np.ndarray,
+                                                                                                       np.ndarray]:
+        target_pollution, predicted_pollution = \
+            list(zip(*[(target_pollution,
+                        self.state_estimation_concatenated(**known_data, target_pollution=target_pollution, **kwargs))
+                       for known_data, target_pollution in loo(observed_stations, observed_pollution, traffic)]))
+
+        return np.concatenate(predicted_pollution, axis=0), np.concatenate(target_pollution, axis=0)
+
+    def calibrate(self, observed_stations, observed_pollution: pd.DataFrame, traffic, **kwargs):
+        if not hasattr(self.lr, "coefs_") or not hasattr(self.lr, "intercept_"):
+            # calibrate models
+            for model in self.models:
+                model.calibrate(observed_stations, observed_pollution, traffic, **kwargs)
+            # find optimal weights for model averaging
+            individual_predictions, target = \
+                self.state_estimation_for_optim(observed_stations, observed_pollution, traffic, **kwargs)
+            not_nan = ~np.any(np.isnan(individual_predictions), axis=1) & ~np.isnan(target)
+            self.lr.fit(individual_predictions[not_nan], target[not_nan])
+        print(f"Models importance: {self.model_importance}")

src/lib/Models/TrueStateEstimationModels/AverageModels.py
@@ -6,7 +6,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 
 from src.lib.Models.BaseModel import BaseModel, loo
-from src.performance_utils import filter_dict
+from src.performance_utils import filter_dict, if_true_str
 
 
 class SummaryModel(BaseModel):
@@ -28,10 +28,16 @@ class SnapshotMeanModel(SummaryModel):
 
 
 class SnapshotWeightedModel(BaseModel):
+    def __init__(self, positive=True, fit_intercept=False, **kwargs):
+        self.positive = positive
+        self.fit_intercept = fit_intercept
+        super().__init__(name=if_true_str(positive, "positive", "_") + if_true_str(fit_intercept, "fit_intercept", "_"),
+                         **kwargs)
 
     def state_estimation(self, observed_stations, observed_pollution, traffic, target_positions,
                          **kwargs) -> np.ndarray:
-        return (observed_pollution @ pd.Series(filter_dict(observed_stations.columns, **self.params))).values
+        return (observed_pollution @ pd.Series(filter_dict(observed_stations.columns, **self.params))).values \
+            .reshape((-1, np.shape(target_positions)[1]))
 
     def calibrate(self, observed_stations, observed_pollution, traffic, **kwargs) -> [np.ndarray, np.ndarray]:
         """
@@ -50,7 +56,7 @@ class SnapshotWeightedModel(BaseModel):
             list(range(len(pollution))), list(map(lambda x: x[0].tolist().index(x[1]), zip(pollution, target)))] = 0
         pollution[np.isnan(pollution)] = 0
 
-        lr = LinearRegression(fit_intercept=False, positive=True).fit(pollution, target)
+        lr = LinearRegression(fit_intercept=self.fit_intercept, positive=self.positive).fit(pollution, target)
         self.set_params(**dict(zip(pollution_columns, lr.coef_)))
         return self
 