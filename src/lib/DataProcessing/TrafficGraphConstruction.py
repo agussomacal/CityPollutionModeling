431adda0b9d172166fd30fd757c0d377e61e030b
somacal <Sorbonne.123>
2023-04-04 19:42:36 +0200

    project_pixels2edges project_traffic_to_edges untested

--- src/lib/DataProcessing/TrafficGraphConstruction.py
+++ src/lib/DataProcessing/TrafficGraphConstruction.py
@@ -1,69 +1,35 @@
-import os
+from collections import defaultdict
+from typing import Dict, Tuple
 
-import numpy as np
 import osmnx as ox
 import pandas as pd
 
-from src.config import traffic_dir
-from src.performance_utils import timeit
+from src.lib.DataProcessing.TrafficProcessing import TRAFFIC_VALUES
+from src.performance_utils import timeit, if_exist_load_else_do
 
 
-def project_pixels2edges(G, traffic_pixels_coords, recalculate=False):
-    filepath = f"{traffic_dir}/pixel2edge.npy"
-    if recalculate or not os.path.exists(filepath):
-        # Projet points into graph edges
-        with timeit("Projecting pixels coords to edges of graph"):
-            index_edges = ox.distance.get_nearest_edges(G, traffic_pixels_coords.loc["long", :],
-                                                        traffic_pixels_coords.loc["lat", :],
-                                                        method=None)
-            np.save(filepath, index_edges)
-    else:
-        with timeit("Time loading pre-processed data:"):
-            index_edges = np.load(filepath)
-    return index_edges
+@if_exist_load_else_do
+def project_pixels2edges(G, traffic_pixels_coords):
+    # Projet points into graph edges
+    with timeit("Projecting pixels coords to edges of graph"):
+        index_edges = ox.distance.get_nearest_edges(G, traffic_pixels_coords.loc["long", :],
+                                                    traffic_pixels_coords.loc["lat", :],
+                                                    method=None)
+        edges_pixels = defaultdict(list)
+        for (geom, u, v), pixel_coord in zip(index_edges, traffic_pixels_coords.columns):
+            edges_pixels[(u, v)].append(pixel_coord)
+    return edges_pixels
 
 
-def project_traffic_to_edges(G, traffic_by_pixel: pd.DataFrame, traffic_pixels_coords, index_edges):
+@if_exist_load_else_do
+def project_traffic_to_edges(G, traffic_by_pixel: pd.DataFrame, edges_pixels: Dict[Tuple, Tuple]):
     """
     traffic_by_pixel: columns: tuple of pixel coords; index: times; values: [0, 1, 2, 3, 4] for [no traffic info, green, yellow...]
-    traffic_pixels_coords: columns: tuple of pixel coords; index: [lat, long]; values: lat and long associate with the pixel
     """
-
-    # Read image traffic data
-    date = t.strftime("%Y-%m-%d-%H-%M-%S")
-    fn = {}
-    fn['green'] = str(traffic_dir) + '/' + date + '_green.png'
-    fn['orange'] = str(traffic_dir) + '/' + date + '_orange.png'
-    fn['red'] = str(traffic_dir) + '/' + date + '_red.png'
-    fn['darkred'] = str(traffic_dir) + '/' + date + '_darkred.png'
-
-    data = {}
-    for key in fn.keys():
-        data[key] = np.sign(np.array(Image.open(fn[key])))
-
-    # Lat-Long of each pixel image
-    LAT, LONG = img_to_latlong()
-
-    # Dictionary traffic[edge]=mean_traffic_on_edge
-    traffic = {}
-    colors = np.array(['green', 'orange', 'red', 'darkred'])
+    traffic = defaultdict(lambda: pd.DataFrame(0, index=traffic_by_pixel.index, columns=TRAFFIC_VALUES.keys()))
     for e in G.edges:
-        for c in range(4):
-            traffic[(e[0], e[1], e[2], c)] = []
+        for pixel in edges_pixels[(e[1], e[2])]:
+            for color, value in TRAFFIC_VALUES.items():
+                traffic[(e[0], e[1], e[2])].loc[:, color] += traffic_by_pixel.loc[:, pixel] == value
 
-    k = 0
-    for i, j in zip(support_indices[0], support_indices[1]):
-        if coordinate_in_box(LAT[i, j], LONG[i, j]):
-            edge = tuple(index_edges[k])
-            for c in range(4):
-                if data[colors[c]][i, j] > 0 and edge in G.edges:
-                    traffic[(edge[0], edge[1], e[2], c)].append(data[colors[c]][i, j])
-            k = k + 1
-
-    mean_traffic = {}
-    for e in G.edges:
-        for c in range(4):
-            ec = (e[0], e[1], e[2], c)
-            mean_traffic[ec] = len(traffic[ec])
-
-    return pd.DataFrame.from_dict(mean_traffic, orient='index', columns=[t])
+    return traffic
