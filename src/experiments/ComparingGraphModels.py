cb2c959e575de2be0c8b29dfa49f7e9621483248
somacal <Sorbonne.123>
2023-05-29 00:20:30 +0200

    Plots for presentation.

--- src/experiments/ComparingGraphModels.py
+++ src/experiments/ComparingGraphModels.py
@@ -1,7 +1,6 @@
 from functools import partial
 from typing import Dict, List
 
-import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 from sklearn.base import RegressorMixin
@@ -18,13 +17,11 @@ from PerplexityLab.DataManager import DataManager
 from PerplexityLab.LabPipeline import LabPipeline
 from PerplexityLab.miscellaneous import NamedPartial, if_true_str, copy_main_script_version
 from PerplexityLab.visualization import generic_plot
-from src.experiments.PreProcess import train_test_model, train_test_averagers, simulation, stations2test, \
-    plot_pollution_map, times_future
+from src.experiments.PreProcess import train_test_model, train_test_averagers, simulation, stations2test
 from src.experiments.config_experiments import shuffle, filter_graph
 from src.lib.Models.BaseModel import ModelsSequenciator, \
     medianse, NONE_OPTIM_METHOD
-from src.lib.Models.TrueStateEstimationModels.AverageModels import SnapshotMeanModel, SnapshotBLUEModel, \
-    SnapshotPCAModel, GlobalMeanModel
+from src.lib.Models.TrueStateEstimationModels.AverageModels import SnapshotMeanModel
 from src.lib.Models.TrueStateEstimationModels.GraphModels import GraphEmissionsNeigEdgeModel
 
 
@@ -217,7 +214,15 @@ if __name__ == "__main__":
                                                                            )))
     lab.define_new_block_of_functions("model",
                                       *list(map(partial(train_test_averagers,
-                                                        aggregator=Pipeline([("LR", LassoCV(selection="random"))])),
+                                                        aggregator=Pipeline([(
+                                                            # "NN",
+                                                            # MLPRegressor(hidden_layer_sizes=(20, 20,),
+                                                            #              activation="logistic",
+                                                            #              learning_rate_init=0.1,
+                                                            #              max_iter=1000))
+                                                            "LR",
+                                                            LassoCV(selection="random"))
+                                                        ])),
                                                 [[model] for model in models + base_models]
                                                 + [models + base_models]
                                                 )))
@@ -228,31 +233,79 @@ if __name__ == "__main__":
         forget=False,
         recalculate=False,
         save_on_iteration=1,
-        station=stations2test  # station_coordinates.columns.to_list()[:2]
+        # station=["BONAP"],  # stations2test
+        station=stations2test
     )
 
-    # ----- Plotting results ----- #
-    plot_pollution_map(data_manager, time=times_future[0], station="OPERA", plot_by=["model", "station"], num_cores=10,
-                       num_points=100, cmap="cividis")
 
-    generic_plot(data_manager, x="station", y="mse", label="model", plot_func=sns.barplot,
-                 sort_by=["mse"],
+    # ----- Plotting results ----- #
+    # plot_pollution_map(data_manager, time=times_future[0], station="OPERA", plot_by=["model", "station"], num_cores=10,
+    #                    num_points=100, cmap="cividis")
+
+    def name_models(model):
+        if model == "LR":
+            return "Graph \n Linear Regression"
+        elif model == "LR_Extra":
+            return "Graph Temp Wind \n Linear Regression"
+        elif model == "NN_Extra":
+            return "Graph Temp Wind \n NeuralNetwork"
+        elif model == "SnapshotMeanModelmean":
+            return "Average in space"
+        elif "Pipeline" in model:
+            return "Ensamble"
+
+
+    models_order = ["Average in space", "Graph \n Linear Regression", "Graph Temp Wind \n Linear Regression",
+                    "Graph Temp Wind \n NeuralNetwork", "Ensamble"]
+    generic_plot(data_manager,
+                 x="models", y="mse",
+                 plot_func=NamedPartial(sns.barplot, orient="vertical", order=models_order, errorbar=("ci", 0)),
+                 # plot_func=sns.barplot,
+                 sort_by=["models"],
                  mse=lambda error: np.sqrt(error.mean()),
-                 station=stations2test,
+                 models=name_models,
                  )
 
-    generic_plot(data_manager, x="l1_error", y="model", plot_func=NamedPartial(sns.boxenplot, orient="horizontal"),
+
+    def name_models(model):
+        if model == "LR":
+            return "Graph Linear Regression"
+        elif model == "LR_Extra":
+            return "Graph Temp Wind Linear Regression"
+        elif model == "NN_Extra":
+            return "Graph Temp Wind NeuralNetwork"
+        elif model == "SnapshotMeanModelmean":
+            return "Average in space"
+        elif "Pipeline" in model:
+            return "Ensamble"
+
+
+    models_order = ["Average in space", "Graph Linear Regression", "Graph Temp Wind Linear Regression",
+                    "Graph Temp Wind NeuralNetwork", "Ensamble"]
+    stations_order = ["OPERA", "BASCH", "PA13", "PA07", "PA18", "ELYS", "PA12", "BONAP"]
+
+    generic_plot(data_manager,
+                 x="station", y="mse", label="models",
+                 plot_func=NamedPartial(sns.barplot, orient="vertical", hue_order=models_order,
+                                        order=stations_order),
+                 sort_by=["models"],
+                 mse=lambda error: np.sqrt(error.mean()),
+                 models=name_models,
+                 )
+
+    generic_plot(data_manager, x="l1_error", y="model",
+                 plot_func=NamedPartial(sns.boxenplot, orient="horizontal", order=stations_order),
                  sort_by=["mse"],
                  l1_error=lambda error: np.abs(error).ravel(),
                  mse=lambda error: np.sqrt(error.mean()),
-                 station=stations2test,
                  # xlim=(0, 20)
                  )
 
     generic_plot(data_manager, x="mse", y="model",
-                 plot_func=NamedPartial(sns.violinplot, orient="horizontal", inner="stick"),
+                 plot_func=NamedPartial(sns.violinplot, orient="horizontal", inner="stick", order=stations_order),
                  sort_by=["mse"],
                  mse=lambda error: np.sqrt(np.nanmean(error)),
+                 models=name_models,
                  # xlim=(0, 100),
                  # model=["SnapshotMeanModelmean", "A+SMM,GMM,SMMTCMN", "GlobalMeanModelmean"]
                  )
@@ -260,8 +313,11 @@ if __name__ == "__main__":
     generic_plot(data_manager, x="model", y="time_to_fit", plot_func=sns.boxenplot)
     generic_plot(data_manager, x="model", y="time_to_estimate", plot_func=sns.boxenplot)
 
-    generic_plot(data_manager, x="station", y="mse", label="model", plot_func=spiderplot,
-                 mse=lambda error: np.sqrt(error.mean()))
+    generic_plot(data_manager, x="station", y="mse", label="models",
+                 plot_func=NamedPartial(spiderplot, hue=models_order),
+                 mse=lambda error: np.sqrt(error.mean()),
+                 models=name_models,
+                 )
 
     generic_plot(data_manager, x="station", y="l1_error", label="model", plot_func=sns.boxenplot,
                  sort_by=["mse"],
